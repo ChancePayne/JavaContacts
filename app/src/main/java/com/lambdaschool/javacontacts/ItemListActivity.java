ae0f81251dacb1738ffb8013eb1783b612bddf83
Chance Payne <chance@lambdaschool.com>
2019-04-24 12:15:10 -0600

    add images to recyclerview

--- app/src/main/java/com/lambdaschool/javacontacts/ItemListActivity.java
+++ app/src/main/java/com/lambdaschool/javacontacts/ItemListActivity.java
@@ -2,6 +2,7 @@ package com.lambdaschool.javacontacts;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,10 +14,12 @@ import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lambdaschool.javacontacts.dummy.DummyContent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,19 +42,6 @@ public class ItemListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
-
-        /*CircularDictionary<Integer, String> data = new CircularDictionary<>(10);
-
-        for(int i = 0; i < data.getLength() + 5; ++i) {
-            data.put(i, Integer.toBinaryString(i));
-        }
-        for(int i = 0; i < data.getLength() + 5; ++i) {
-            System.out.println(data.get());
-        }
-        System.out.println(data.get(1));
-        System.out.println(data.get(14));
-        System.out.println(data.get(7));*/
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -80,22 +70,26 @@ public class ItemListActivity extends AppCompatActivity {
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, mTwoPane));
     }
 
     public static class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final ItemListActivity             mParentActivity;
-        private final List<DummyContent.DummyItem> mValues;
+        public static final int CACHE_SIZE = 15;
+
+        private final ItemListActivity mParentActivity;
+        private final List<Contact> mValues;
+        private final CircularDictionary<String, Bitmap> imageCache;
         private final boolean                      mTwoPane;
+
         private final View.OnClickListener         mOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
+                Contact item = (Contact) view.getTag();
                 if (mTwoPane) {
                     Bundle arguments = new Bundle();
-                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.id);
+                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.getName());
                     ItemDetailFragment fragment = new ItemDetailFragment();
                     fragment.setArguments(arguments);
                     mParentActivity.getSupportFragmentManager().beginTransaction()
@@ -104,7 +98,7 @@ public class ItemListActivity extends AppCompatActivity {
                 } else {
                     Context context = view.getContext();
                     Intent  intent  = new Intent(context, ItemDetailActivity.class);
-                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id);
+                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.getName());
 
                     context.startActivity(intent);
                 }
@@ -112,11 +106,27 @@ public class ItemListActivity extends AppCompatActivity {
         };
 
         SimpleItemRecyclerViewAdapter(ItemListActivity parent,
-                                      List<DummyContent.DummyItem> items,
                                       boolean twoPane) {
-            mValues = items;
+            mValues = new ArrayList<>();
+            imageCache = new CircularDictionary<>(CACHE_SIZE);
+            getContacts();
             mParentActivity = parent;
             mTwoPane = twoPane;
+        }
+
+        private void getContacts() {
+            ContactDao.getContacts(new ContactDao.ContactCallback() {
+                @Override
+                public void processContacts(List<Contact> contacts) {
+                    mValues.addAll(contacts);
+                    mParentActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
+            });
         }
 
         @Override
@@ -128,10 +138,34 @@ public class ItemListActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
+            final Contact contact = mValues.get(position);
+            holder.mIdView.setText(contact.getName());
+            holder.mContentView.setText(contact.getPhone());
 
-            holder.itemView.setTag(mValues.get(position));
+            final Bitmap contactImage = imageCache.get(contact.getImageThumbnail());
+            if(contactImage == null) {
+                // get image
+                holder.mImageView.setImageDrawable(mParentActivity.getDrawable(R.drawable.ic_launcher_background));
+                NetworkAdapter.backgroundBitmapFromUrl(contact.getImageThumbnail(), new NetworkAdapter.NetworkImageCallback() {
+                    @Override
+                    public void processImage(final Bitmap image) {
+                        imageCache.put(contact.getImageThumbnail(), image);
+
+                        mParentActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                holder.mImageView.setImageBitmap(image);
+                            }
+                        });
+                    }
+                });
+            } else {
+                // set image
+                holder.mImageView.setImageBitmap(contactImage);
+            }
+
+
+            holder.itemView.setTag(contact);
             holder.itemView.setOnClickListener(mOnClickListener);
         }
 
@@ -143,11 +177,13 @@ public class ItemListActivity extends AppCompatActivity {
         class ViewHolder extends RecyclerView.ViewHolder {
             final TextView mIdView;
             final TextView mContentView;
+            final ImageView mImageView;
 
             ViewHolder(View view) {
                 super(view);
                 mIdView = (TextView) view.findViewById(R.id.id_text);
                 mContentView = (TextView) view.findViewById(R.id.content);
+                mImageView = view.findViewById(R.id.image);
             }
         }
     }
