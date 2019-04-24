980b5addb28fd18ebe7cca6c58803f8c67b17083
VivekV95 <vivekv210@gmail.com>
2019-04-24 11:59:55 -0500

    add generic for key type

--- app/src/main/java/com/lambdaschool/javacontacts/CircularDictionary.java
+++ app/src/main/java/com/lambdaschool/javacontacts/CircularDictionary.java
@@ -1,19 +1,19 @@
 package com.lambdaschool.javacontacts;
 
-public class CircularDictionary<E> {
+public class CircularDictionary<K, E> {
 
     private E[] data;
-    private String[] keys;
+    private K[] keys;
     private int pointer, length;
 
     public CircularDictionary(int size) {
         data = (E[]) new Object[size];
-        keys = new String[size];
+        keys = (K[]) new Object[size];
         length = size;
         pointer = 0;
     }
 
-    public void put(String key, E item) {
+    public void put(K key, E item) {
         keys[pointer] = key;
         data[pointer] = item;
         increasePointer();
@@ -25,7 +25,7 @@ public class CircularDictionary<E> {
         return element;
     }
 
-    public E get(String key) {
+    public E get(K key) {
         int index = -1;
         for (int i = 0; i < keys.length; i++) {
             if (key.equals(keys[i])) {
